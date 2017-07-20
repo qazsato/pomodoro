bb1c286486e31efbe9a0ee85fa93de0e28d7bf19
qazsato <qazsato@gmail.com>
2017-07-20 20:50:30 +0900

    初期時間及びリセット時に設定する時間を定数から参照するように修正

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,34 @@
-const pomodoro = new Pomodoro(function (remainTime) {
-  const time = Math.round(this.remainTime / 1000);
-  const min = Math.floor(time / 60);
-  let sec = time % 60;
-  sec = sec < 10 ? `0${sec}`: sec;
-  document.querySelectorAll('.min')[0].innerText = `${min}`;
-  document.querySelectorAll('.sec')[0].innerText = `${sec}`;
-});
+window.onload = () => {
+  const pomodoro = new Pomodoro(function (remainSec) {
+    const min = Math.floor(remainSec / 60);
+    let sec = remainSec % 60;
+    sec = sec < 10 ? `0${sec}`: sec;
+    document.querySelectorAll('.min')[0].innerText = `${min}`;
+    document.querySelectorAll('.sec')[0].innerText = `${sec}`;
+    if (remainSec <= 0) {
+      setTimeout(() => alert('end')); // HACK 0:00時点のDOM書き換えがalertで阻止され描画されないためtimeoutで逃がす
+    }
+  });
+  setDefaultTime();
 
-const startBtn = document.querySelectorAll('#start')[0];
-const stopBtn = document.querySelectorAll('#stop')[0];
-const resetBtn = document.querySelectorAll('#reset')[0];
-startBtn.addEventListener('click', () => {
-  pomodoro.start();
-});
-stopBtn.addEventListener('click', () => {
-  pomodoro.stop();
-});
-resetBtn.addEventListener('click', () => {
-  document.querySelectorAll('.min')[0].innerText = '25';
-  document.querySelectorAll('.sec')[0].innerText = '00';
-  pomodoro.reset();
-});
+  const startBtn = document.querySelectorAll('#start')[0];
+  const stopBtn = document.querySelectorAll('#stop')[0];
+  const resetBtn = document.querySelectorAll('#reset')[0];
+  startBtn.addEventListener('click', () => {
+    pomodoro.start();
+  });
+  stopBtn.addEventListener('click', () => {
+    pomodoro.stop();
+  });
+  resetBtn.addEventListener('click', () => {
+    setDefaultTime();
+    pomodoro.reset();
+  });
+};
+
+function setDefaultTime() {
+  const min = Math.floor(POMODORO_TIME / 1000 / 60);
+  let sec = POMODORO_TIME / 1000 % 60;
+  document.querySelectorAll('.min')[0].innerText = min;
+  document.querySelectorAll('.sec')[0].innerText = sec < 10 ? `0${sec}`: sec;
+}
