335c8f545fb405a61dd2d694e5a945ab68346a30
qazsato <qazsato@gmail.com>
2017-07-21 00:17:21 +0900

    休憩時間(short/long)の切り替え対応

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 window.onload = () => {
-  const pomodoro = new Pomodoro(function (remainSec) {
+  const pomodoro = new Pomodoro(function (status, remainSec) {
     const min = Math.floor(remainSec / 60);
     let sec = remainSec % 60;
     sec = sec < 10 ? `0${sec}`: sec;
     document.querySelectorAll('.min')[0].innerText = `${min}`;
     document.querySelectorAll('.sec')[0].innerText = `${sec}`;
-    if (remainSec <= 0) {
-      setTimeout(() => alert('end')); // HACK 0:00時点のDOM書き換えがalertで阻止され描画されないためtimeoutで逃がす
-    }
+    // if (remainSec <= 0) {
+    //   setTimeout(() => alert('end')); // HACK 0:00時点のDOM書き換えがalertで阻止され描画されないためtimeoutで逃がす
+    // }
   });
   setDefaultTime();
 
@@ -27,8 +27,8 @@ window.onload = () => {
 };
 
 function setDefaultTime() {
-  const min = Math.floor(POMODORO_TIME / 1000 / 60);
-  let sec = POMODORO_TIME / 1000 % 60;
+  const min = Math.floor(TIME.POMODORO / 60);
+  let sec = TIME.POMODORO % 60;
   document.querySelectorAll('.min')[0].innerText = min;
   document.querySelectorAll('.sec')[0].innerText = sec < 10 ? `0${sec}`: sec;
 }
--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -1,13 +1,29 @@
-const POMODORO_TIME = 25 * 60 * 1000;
-const BREAK_SHORT_TIME = 5 * 60 * 1000;
-const BREAK_LONG_TIME = 10 * 60 * 1000;
+const TIME = {
+  // POMODORO: 25 * 60,
+  // BREAK_SHORT: 5 * 60,
+  // BREAK_LONG: 30 * 60
+  POMODORO: 3,
+  BREAK_SHORT: 3,
+  BREAK_LONG: 3
+};
+
+const STATUS = {
+  POMODORO: 'pomodoro',
+  BREAK_SHORT: 'break_short',
+  BREAK_LONG: 'break_long'
+};
 
 class Pomodoro {
   constructor(callback) {
     this.callback = callback;
     this.intervalID = null;
     this.prevTime = null;
-    this.remainTime = POMODORO_TIME;
+    this.count = {
+      pomodoro: 0,
+      break: 0
+    };
+    this.status = this._getStatus();
+    this.remainTime = this._getRemainTime();
   }
 
   start() {
@@ -24,18 +40,57 @@ class Pomodoro {
   }
 
   reset() {
-    this.remainTime = POMODORO_TIME;
+    this.remainTime = this._getRemainTime();
   }
 
   _count() {
     if (!this.prevTime) {
-      this.prevTime = new Date().getTime();
+      this.prevTime = this._getTime();
       return;
     }
-    const nextTime = new Date().getTime();
+    const nextTime = this._getTime();
     const diffTime = nextTime - this.prevTime;
     this.remainTime = (this.remainTime - diffTime) < 0 ? 0 : (this.remainTime - diffTime);
-    this.callback(Math.round(this.remainTime / 1000));
+    this.callback(this.status, this.remainTime);
     this.prevTime = nextTime;
+    if (this.remainTime === 0) {
+      if (this.status === STATUS.POMODORO) {
+        this.count.pomodoro++;
+      } else {
+        this.count.break++;
+      }
+      this.status = this._getStatus();
+      this.remainTime = this._getRemainTime();
+    } else {
+      
+    }
+  }
+
+  _getTime() {
+    return Math.round(new Date().getTime() / 1000);
+  }
+
+  _getStatus() {
+    let status;
+    if (this.count.pomodoro === this.count.break) {
+      status = STATUS.POMODORO;
+    } else if (this.count.pomodoro % 4 === 0) {
+      status = STATUS.BREAK_LONG;
+    } else if (this.count.pomodoro > this.count.break) {
+      status = STATUS.BREAK_SHORT;
+    }
+    return status;
+  }
+
+  _getRemainTime() {
+    let time;
+    if (this.status === STATUS.POMODORO) {
+      time = TIME.POMODORO;
+    } else if (this.status === STATUS.BREAK_SHORT) {
+      time = TIME.BREAK_SHORT;
+    } else if (this.status === STATUS.BREAK_LONG) {
+      time = TIME.BREAK_LONG;
+    }
+    return time;
   }
 }
